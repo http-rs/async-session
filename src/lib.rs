82a1afd0fad5d54f081244fe9b195d13d375ba67
Yoshua Wuyts <yoshuawuyts@gmail.com>
2020-02-06 14:12:45 +0100

    .

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+//! Async session support with pluggable middleware
+//!
+//! # Examples
+//!
+//! ```
+//! // tbi
+//! ```
+
+#![forbid(unsafe_code, future_incompatible, rust_2018_idioms)]
+#![deny(missing_debug_implementations, nonstandard_style)]
+#![warn(missing_docs, missing_doc_code_examples, unreachable_pub)]
